b3a8454819a577044de486d123740d8accb91f11
iki55 <aleksovska.isidora@gmail.com>
2023-12-24 11:50:13 +0100

    Add first java program

---
+++ src/proekt_20/MyClass.java
@@ -0,0 +1,12 @@
+package proekt_20;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	Vehicle m = new Vehicle();
+	System.out.println(m);
+	Vehicle m1=new Vehicle("blue");
+	System.out.println(m1);
+	}
+
+}
---
+++ src/proekt_20/Vehicle.java
@@ -0,0 +1,16 @@
+package proekt_20;
+
+public class Vehicle {
+	private String color;
+	Vehicle (){
+		color="red";
+	}
+	Vehicle (String r){
+	this.color = r;
+	}
+	
+	public String toString() {
+		return color;
+	}
+
+}
